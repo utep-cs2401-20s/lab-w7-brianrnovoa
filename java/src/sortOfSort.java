fedeaddc8544aa244331a5d9181629bb37527336
Brian Novoa <brianrnovoa@gmail.com>
2020-03-04 14:48:49 -0700

    made changes, still testing different methods

--- java/src/sortOfSort.java
+++ java/src/sortOfSort.java
@@ -1,22 +1,43 @@
 public class sortOfSort {
 
     public static void sortOfSort(int[] array) {
-        int count = 0;
-        int temp;
+        int elements = array.length;
+        int index = 0;
+        sortOfSortRecursive(array, elements, index);
+    }
 
-        for (int i = 0; i < array.length; i++) {
-            if (count <= 2) {
-                if(i + 1 < array.length) {
-                    if (array[i] > array[i + 1]) {
-                        temp = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = temp;
-                        count++;
-                    }
-                }
+    public static void swapElements(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void sortOfSortRecursive(int[] array, int elements, int index) {
+        int countSwaps = 0;
+        int superCount = 0;
+
+        if(index < elements - 1) {
+            if (array[index] > array[index + 1]) {
+                swapElements(array, index, index + 1);
+            }
+            else {
+                sortOfSortRecursive(array, elements - 1, index + 1);
             }
         }
-
+        if(elements - 1 > 1) {
+            sortOfSortRecursive(array, elements - 1, index + 1);
+        }
 
     }
+
+
+
+
+
+
+
+
+
+
+// end of class //
 }
